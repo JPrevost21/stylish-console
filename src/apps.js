8856a98015fe7c88ee1e3e06a4b74f45eeb369f1
Joseph <joeprevost21@gmail.com>
2017-07-21 12:52:05 -0400

    Added color and bold to msg when consoled

--- src/apps.js
+++ src/apps.js
@@ -1,11 +1,14 @@
 var stylishConsole = {
     success: function(msg) {
-        console.log('✅ ' + msg);
+        console.log('%c ✅ ' + msg, 'color: green; font-weight: bold;');
     },
     error: function(msg) {
-        console.log('❌ ' + msg);
+        console.log('%c ❌ ' + msg, 'color: red; font-weight: bold;');
     },
     info: function(msg) {
-        console.log('⚠️ '  + msg);
+        console.log('%c ⚠️ '  + msg, 'color: orange; font-weight: bold;');
     }
 };
+
+
+'%c the green hulk got mad!', 'color: green; font-weight: bold;'
