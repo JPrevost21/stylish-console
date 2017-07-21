0b2c3800440e043b229ebe711ef550e882dd15ea
Joseph <joeprevost21@gmail.com>
2017-07-21 12:15:19 -0400

    Started project / added apps.js and code

---
+++ apps.js
@@ -0,0 +1,11 @@
+var prettyConsole = {
+    success: function(msg) {
+        console.log('✅ ' + msg)
+    },
+    error: function(msg) {
+        console.log('❌ ' + msg)
+    },
+    info: function(msg) {
+        console.log('⚠️ '  + msg)
+    }
+};
